fa8b51fe937786a127d40ba5219491be5c9a24fc
François de Metz <francois@2metz.fr>
2010-12-28 09:02:22 +0100

    first test \o/

---
+++ test/server.test.js
@@ -0,0 +1,46 @@
+/**
+ * Zizanie
+ * Copyright (C) 2010 François de Metz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var express = require('express');
+var zizanie = require('../lib/zizanie/server').zizanie;
+var models = require('../lib/zizanie/models');
+var assert = require('assert');
+
+function getInitConfig(callback) {
+    require('../lib/zizanie/config').getConfig(function(config) {
+        // override default database
+        config.mongodb.db_name = 'zizanie-test';
+        var mongoose = models.configure(config);
+        callback(config, mongoose);
+    });
+}
+
+/**
+ * Test server
+ */
+module.exports = {
+    'index anonymous': function() {
+        var server = express.createServer();
+        getInitConfig(function(config, models) {
+            new zizanie(server, config, models).init();
+            assert.response(server,
+                            {url: '/', method: 'GET'},
+                            {status: 200, body: /input/g },
+                            function() { models.close();});
+        });
+    }
+};
