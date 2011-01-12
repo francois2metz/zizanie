0064b018c0951c5a993ea8a3dc1dd817ecee2520
François de Metz <francois@2metz.fr>
2011-01-12 21:04:20 +0100

    added deployment script

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+from __future__ import with_statement
+from fabric.api import *
+
+env.user = 'pazzis'
+env.port = 2001
+env.hosts = ['beta.pazzis.com:2001']
+
+BASE_DIR = '/home/pazzis/zizanie'
+
+def prepare_deploy():
+    """
+    Create archive
+    """
+    local('tar -czf /tmp/zizanie.tgz . --exclude-vcs', capture=False)
+
+def deploy():
+    """
+    Deploy to server
+    """
+    put('/tmp/zizanie.tgz', BASE_DIR)
+    with cd(BASE_DIR):
+        run('tar xzf zizanie.tgz')
+
+def start_server():
+    """
+    TODO : Restart node
+    """
+    with cd(BASE_DIR):
+        run('node lib/zizanie/zizanie.js')
