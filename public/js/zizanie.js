b5a36ef9e61682643cf85042933445be71825de5
François de Metz <francois@2metz.fr>
2011-01-12 02:14:41 +0100

    add class hide_of_facebook_connected

--- public/js/zizanie.js
+++ public/js/zizanie.js
@@ -1,21 +1,4 @@
 (function() {
-    var IndexView = Backbone.View.extend({
-        events: {
-//            "submit form" : "login"
-        },
-
-        login: function(e) {
-            e.preventDefault();
-            $.post('/user/sign_in', $(e.target).serialize(), function(data) {
-
-            });
-        },
-
-        render: function() {
-        }
-
-    });
-
     var ZizanieMainController = Backbone.Controller.extend({
         routes: {
             "":                 "index", // #/
@@ -24,23 +7,23 @@
         initialize: function() {
             window.fbAsyncInit = function() {
                 FB.init({
-                    appId  : '133534500035590',
+                    appId  : '133534500035590', // TODO: make this one customisable
                     status : true, // check login status
                     cookie : true, // enable cookies to allow the server to access the session
                     xfbml  : true  // parse XFBML
                 });
                 FB.Event.subscribe('auth.sessionChange', function(response) {
                     if (response.session) {
-                        $('.facebook-login').hide();
+                        $('.hide_if_facebook_connected').hide();
                     } else {
-
+                        $('.hide_if_facebook_connected').show();
                     }
                 });
                 FB.getLoginStatus(function(response) {
                     if (response.session) {
-                        $('.facebook-login').hide();
+                        $('.hide_if_facebook_connected').hide();
                     } else {
-
+                        $('.hide_if_facebook_connected').show();
                     }
                 });
                 FB.XFBML.parse();
